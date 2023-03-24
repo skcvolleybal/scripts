2c534c534647f3578058ad3b9690a158a214e4e9
KM Schellens <k.schellens@gmail.com>
2023-03-24 14:02:47 +0100

    Temporarily disable sentry
    Because it doesn't play nice with the Joomla autoloader

--- CommissieMembers.php
+++ CommissieMembers.php
@@ -1,9 +1,9 @@
 <?php
-require 'vendor/autoload.php';
-$env = json_decode(file_get_contents("../../env.json"));
+// require 'vendor/autoload.php';
+// $env = json_decode(file_get_contents("../../env.json"));
 
-\Sentry\init(['dsn' => 'https://45df5d88f1084fcd96c8ae9fa7db50c7@o4504883122143232.ingest.sentry.io/4504883124240384',
-'environment' => $env->Environment ]);
+// \Sentry\init(['dsn' => 'https://45df5d88f1084fcd96c8ae9fa7db50c7@o4504883122143232.ingest.sentry.io/4504883124240384',
+// 'environment' => $env->Environment ]);
 
   $db = JFactory::getDbo();
   jimport( 'joomla.access.access' );
--- NevoboApi.class.php
+++ NevoboApi.class.php
@@ -1,11 +1,11 @@
 <?php
 require_once 'libs/SimplePie1.3/autoloader.php';
 
-require 'vendor/autoload.php';
+// require 'vendor/autoload.php';
 
-$env = json_decode(file_get_contents("../../env.json"));
+// $env = json_decode(file_get_contents("../../env.json"));
 
-\Sentry\init(['dsn' => 'https://45df5d88f1084fcd96c8ae9fa7db50c7@o4504883122143232.ingest.sentry.io/4504883124240384',
+// \Sentry\init(['dsn' => 'https://45df5d88f1084fcd96c8ae9fa7db50c7@o4504883122143232.ingest.sentry.io/4504883124240384',
 'environment' => $env->Environment ]);
 
 
